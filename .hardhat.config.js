27e092c50318c15e43da72ea0d9c5e0dc20983c5
Aethera BioSync <avatariamfoundation@gmail.com>
2026-01-02 19:06:38 +0200

    Update .hardhat.config.js

--- .hardhat.config.js
+++ .hardhat.config.js
@@ -1,12 +1,29 @@
-require("@nomiclabs/hardhat-waffle");
+require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
 module.exports = {
-  solidity: "0.8.19",
-  networks: {
-    testnet: {
-      url: process.env.BNB_RPC_URL,
-      accounts: [process.env.PRIVATE_KEY],
-    },
+  solidity: {
+    version: "0.8.21",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      },
+      viaIR: true
+    }
   },
+
+  networks: {
+    hardhat: {},
+
+    bnbTestnet: {
+      url: process.env.BNB_RPC_URL || "https://data-seed-prebsc-1-s1.binance.org:8545/",
+      accounts: process.env.DEPLOYER_PRIVATE_KEY
+        ? [process.env.DEPLOYER_PRIVATE_KEY]
+        : [],
+      chainId: 97,
+      gasPrice: 20_000_000_000,
+      gas: 8_000_000
+    }
+  }
 };
